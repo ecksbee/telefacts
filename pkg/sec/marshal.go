5104e71c95a7b52307de08e08b8a0cd1faa0bb6f
Joshua Nario <joshua.nario@hotmail.com>
2021-03-28 22:19:16 -0400

    SECTickers

--- pkg/sec/marshal.go
+++ pkg/sec/marshal.go
@@ -1,18 +1,34 @@
 package sec
 
 import (
+	"encoding/json"
+	"fmt"
+
+	"ecksbee.com/telefacts/internal/actions"
 	"ecksbee.com/telefacts/pkg/renderables"
 )
 
 func names() map[string]map[string]string {
-	//todo fetch https://www.sec.gov/files/company_tickers.json
-	//todo cache
 	ret := make(map[string]map[string]string)
+	b, err := actions.Scrape(`https://www.sec.gov/files/company_tickers.json`)
+	if err != nil {
+		return ret
+	}
+	type SECTickers map[string]struct {
+		CIK    int    `json:"cik_str"`
+		Ticker string `json:"ticker"`
+		Title  string `json:"title"`
+	}
+	var f SECTickers
+	err = json.Unmarshal(b, &f)
+	if err != nil {
+		return ret
+	}
 	cik := "http://www.sec.gov/CIK"
 	ret[cik] = make(map[string]string)
-	ret[cik]["0001445305"] = "WORKIVA INC"
-	ret[cik]["0000069891"] = "FILER DIRECT CORP"
-	ret[cik]["0000843006"] = "NATIONAL BEVERAGE CORP"
+	for _, obj := range f {
+		ret[cik][fmt.Sprintf("%010d", obj.CIK)] = obj.Title
+	}
 	return ret
 }
 
@@ -31,7 +47,7 @@ func Marshal(workingDir string, slug string) ([]byte, error) {
 	if err != nil {
 		return nil, err
 	}
-	//todo check if slug is passthrough
+	//todo check if slug is passthrough to a file
 	hash := slug
 	return renderables.MarshalRenderable(hash, names(), h)
 }
