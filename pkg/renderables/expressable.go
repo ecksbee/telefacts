5679431cab5eedc6823c60b78e8839d8a082e27f
Joshua Nario <joshuanario@users.noreply.github.com>
2022-05-14 18:53:01 -0400

    expressable

--- pkg/renderables/expressable.go
+++ pkg/renderables/expressable.go
@@ -3,10 +3,32 @@ package renderables
 import "ecksbee.com/telefacts/pkg/hydratables"
 
 type Expressable struct {
+	Href    string
+	Labels  LabelPack
+	Context struct {
+		Period LanguagePack
+		VoidQuadrant
+		ContextualMemberGrid
+	}
+	Measurement string
+	Precision   string
+	Footnotes   []string
+	Value       string
 }
 
 func getExpressions(h *hydratables.Hydratable) (map[string]Expressable, error) {
 	ret := make(map[string]Expressable)
-	//todo
+	source := h.Document
+	if source != nil {
+		for _, item := range source.Expressions {
+			id := item.ID
+			if id == "" {
+				continue
+			}
+			ret[id] = Expressable{
+				//todo
+			}
+		}
+	}
 	return ret, nil
 }
