72f2527d325a1a8f7e391938fb9cf431c6ed500e
Joshua Nario <joshuanario@users.noreply.github.com>
2022-10-03 23:36:25 -0400

    unfudge roletype

--- pkg/serializables/schemaFile.go
+++ pkg/serializables/schemaFile.go
@@ -46,7 +46,7 @@ type SchemaFile struct {
 					XMLName  xml.Name
 					XMLAttrs []xml.Attr `xml:",any,attr"`
 					CharData string     `xml:",chardata"`
-				} `xml:"roleType"`
+				} `xml:"definition"`
 				UsedOn []struct {
 					XMLName  xml.Name
 					XMLAttrs []xml.Attr `xml:",any,attr"`
